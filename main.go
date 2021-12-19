53b6b6a2eecfa41383780391a36efdb383bcbf78
Aditya Saxena <60032144+asaxena012@users.noreply.github.com>
2021-12-19 15:57:25 +0530

    setter made

--- main.go
+++ main.go
@@ -9,6 +9,14 @@ import (
 func main() {
 	fmt.Println("hello world")
 	p := organization.NewPerson("aadi", "saxena")
+	err := p.SetTwitterHandle("@aadi")
+
 	fmt.Println(p.ID())
 	fmt.Println(p.Fullname())
+
+	if(err != nil){
+		fmt.Println(err)
+	}
+
+	fmt.Println(p.TwitterHandle())
 }
--- organization/person.go
+++ organization/person.go
@@ -1,5 +1,10 @@
 package organization
 
+import (
+	"errors"
+	"strings"
+)
+
 type Identifiable interface {
 	ID() string
 }
@@ -7,6 +12,7 @@ type Identifiable interface {
 type Person struct {
 	firstName string
 	lastName string
+	twitterHandle string
 }
 
 func NewPerson (FirstName, LastName string) Person {
@@ -16,10 +22,23 @@ func NewPerson (FirstName, LastName string) Person {
 	}
 }
 
-func (p Person) Fullname() string {
+func (p *Person) SetTwitterHandle(handle string) error {
+	if(!strings.HasPrefix(handle, "@")) {
+		return errors.New("twitter handle must start with an @ character")
+	}
+
+	p.twitterHandle = handle
+	return nil
+}
+
+func (p *Person) TwitterHandle() string {
+	return p.twitterHandle
+}
+
+func (p *Person) Fullname() string {
 	return p.firstName + p.lastName
 }
 
-func (p Person) ID() string {
+func (p *Person) ID() string {
 	return "1234"
 }
